315fa041c793818fdf0c7c5a8cdf1f346b01c2a0
Alexei Sholik <alcosholik@gmail.com>
2014-05-12 15:05:04 +0300

    Clean up mix.exs

--- mix.exs
+++ mix.exs
@@ -8,18 +8,10 @@ defmodule Commando.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: []]
   end
 
-  # List all dependencies in the format:
-  #
-  # {:foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
